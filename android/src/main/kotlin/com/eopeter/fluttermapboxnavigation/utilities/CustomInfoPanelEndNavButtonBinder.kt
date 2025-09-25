685cd2d4c7c31c011bd1ffdbb16ae8f9be7fa23b
guilherme21alcantara <guilhermeafonsoalcantara@gmail.com>
2025-09-25 16:56:52 -0300

    devolvendo o X da navegacao

--- android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/utilities/CustomInfoPanelEndNavButtonBinder.kt
+++ android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/utilities/CustomInfoPanelEndNavButtonBinder.kt
@@ -2,25 +2,41 @@ package com.eopeter.fluttermapboxnavigation.utilities
 
 import android.app.Activity
 import android.view.ViewGroup
+import com.eopeter.fluttermapboxnavigation.activity.NavigationLauncher
+import com.eopeter.fluttermapboxnavigation.models.MapBoxEvents
 import com.mapbox.navigation.core.MapboxNavigation
 import com.mapbox.navigation.core.lifecycle.MapboxNavigationObserver
+import com.mapbox.navigation.dropin.R
+import com.mapbox.navigation.dropin.internal.extensions.updateMargins
 import com.mapbox.navigation.ui.base.lifecycle.UIBinder
 import com.mapbox.navigation.ui.base.lifecycle.UIComponent
+import com.mapbox.navigation.ui.base.view.MapboxExtendableButton
 
 class CustomInfoPanelEndNavButtonBinder(
-    private val activity: Activity
+    val activity: Activity
 ) : UIBinder {
-
     override fun bind(viewGroup: ViewGroup): MapboxNavigationObserver {
-        // Remove qualquer botão ou view anterior
+        val button = MapboxExtendableButton(
+            viewGroup.context,
+            null,
+            R.style.DropInStyleExitButton
+        )
+        button.iconImage.setImageResource(R.drawable.mapbox_ic_stop_navigation)
         viewGroup.removeAllViews()
-
-        // Não adiciona nenhum botão — painel ficará vazio
+        viewGroup.addView(button)
+        button.updateMargins(
+            right = button.resources.getDimensionPixelSize(R.dimen.mapbox_infoPanel_paddingEnd)
+        )
 
         return object : UIComponent() {
             override fun onAttached(mapboxNavigation: MapboxNavigation) {
                 super.onAttached(mapboxNavigation)
-                // Nenhuma ação de clique, já que não há botão
+                button.setOnClickListener {
+                    mapboxNavigation.stopTripSession()
+                    PluginUtilities.sendEvent(MapBoxEvents.NAVIGATION_CANCELLED)
+                        PluginUtilities.sendFreeDriveStartedEvent() // ✅ dispara o novo evento
+
+                }
             }
         }
     }
